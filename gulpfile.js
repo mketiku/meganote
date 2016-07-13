80a2e35fed905aacd168d744873872d5a0fc6dbc
michael ketiku <mketiku@gmail.com>
2016-07-13 09:43:50 -0400

    add my name to bower, gulp and package.json components

--- gulpfile.js
+++ gulpfile.js
@@ -9,43 +9,51 @@
   var connect = require('gulp-connect');
 
   gulp.task('bundle', bundle);
-  gulp.task('start-web-server', startWebServer);
   gulp.task('watch', watch);
-  gulp.task('default', ['bundle', 'start-web-server', 'watch']);
-
-  ////////////////////
-
-  var jsFiles = [
+  gulp.task('start-web-server' ,startWebServer)
+  gulp.task('default', ['bundle','start-web-server','watch']);
+// gulp functions: gulp dest, watch, 
+ 
+var jsFiles = [
+//  hold all the jsFiles
+        // array of patterns, 
     'app/**/*.js',
     '!app/bower_components/**/*',
     '!app/content/bundle.js'
-  ];
-
-  function bundle() {
-    return gulp.src(jsFiles)
-      .pipe(order([
-        'app/app.module.js',              // put the main module first
-        'app/**/*.module.js',             // followed by all other modules
-        'app/**/*.js'                     // and all other JS files
-      ], { base: './' }))
-      .pipe(plumber())                    // restart gulp on error
-      .pipe(sourcemaps.init())            // let sourcemaps watch this pipeline
-      .pipe(babel({
-        presets: ['es2015']
-      }))                                 // transpile into ES5 for browsers
-      .pipe(concat('bundle.js'))          // concatenate all JS files
-      .pipe(sourcemaps.write('.'))        // emit the .map file for debugging
-      .pipe(gulp.dest('app/content'));
+    // ignore app-bower components  and bundle.js
+];
+// get everything together 
+ function bundle() {
+     return gulp.src(jsFiles)
+     .pipe(order([
+             'app/app.module.js',
+             //put main module first followed by other JS files
+             'app/**/*.module.js',
+             'app/**/*.js',
+     ], { base: './' }))
+     .pipe(plumber()) //restart gulp on error 
+     .pipe(sourcemaps.init())  //let sourcemaps watch this pipe
+     .pipe(babel()) //transpile into es5 for browsers
+     .pipe(concat('bundle.js')) //smash all js files into bundle.js
+     .pipe(sourcemaps.write('')) //emits the .map file for debugging
+    //  source maps maintains the original file for console.log errors
+    .pipe(babel({
+      presets:['es2015']
+    }))
+    .pipe(gulp.dest('app/content'));
   }
 
+    //   start our web server
   function startWebServer() {
-    connect.server({
-      root: 'app',
-      port: 8000
-    });
+        connect.server({
+            root:'app',
+            port: 8000
+            // serve the server to run from here
+        })
   }
-
-  function watch() {
-    gulp.watch(['app/**/*', 'gulpfile.js'], ['bundle']);
+  function watch(){
+    // watch for changes in certain files and when they change run, a series of tasks
+    gulp.watch(['app/**/*','gulpfile.js'],['bundle']);
+    // any file in any subdirectory of app
   }
 })();
